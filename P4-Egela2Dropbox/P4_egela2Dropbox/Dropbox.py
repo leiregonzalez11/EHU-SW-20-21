16325b3a9063300f23a4e73415fa2c1c752c93c5
Leire <leiregonzalezlopez992gmail.com>
2021-05-29 17:09:58 +0200

    Last update from proyect. Proyect finished ✅

---
+++ P4-Egela2Dropbox/P4_egela2Dropbox/Dropbox.py
@@ -0,0 +1,219 @@
+from tkinter import messagebox
+from wsgiref import headers
+
+import requests
+import urllib
+import webbrowser
+from socket import AF_INET, socket, SOCK_STREAM
+import json
+import helper
+
+app_key = 'kfacoosqtm5n9zp'
+app_secret = 'bgyqh2dt1xmpq6h'
+server_addr = "localhost"
+server_port = 8090
+redirect_uri = "http://" + server_addr + ":" + str(server_port)
+
+class Dropbox:
+    _access_token = ""
+    _path = "/"
+    _files = []
+    _root = None
+    _msg_listbox = None
+
+    def __init__(self, root):
+        self._root = root
+
+
+    def local_server(self):
+
+        # 8090. portuan entzuten dagoen zerbitzaria sortu
+        server_socket = socket(AF_INET, SOCK_STREAM)
+        server_socket.bind((server_addr, server_port))
+        server_socket.listen(1)
+        print("\tLocal server listening on port " + str(server_port))
+
+        # nabitzailetik 302 eskaera jaso
+        client_connection, client_address = server_socket.accept()
+        eskaera = client_connection.recv(1024)
+        print("\tRequest from the browser received at local server:")
+        print (eskaera)
+
+
+        # eskaeran "auth_code"-a bilatu
+        # --> aquiii
+        lehenengo_lerroa = eskaera.decode('UTF8').split('\n')[0]
+        aux_auth_code = lehenengo_lerroa.split(' ')[1]
+        auth_code = aux_auth_code[7:].split('&')[0]
+        print ("\tauth_code: " + auth_code)
+
+        # erabiltzaileari erantzun bat bueltatu
+        http_response = "HTTP/1.1 200 OK\r\n\r\n" \
+                        "<html>" \
+                        "<head><title>Proba</title></head>" \
+                        "<body>The authentication flow has completed. Close this window.</body>" \
+                        "</html>"
+        client_connection.sendall(http_response.encode(encoding="utf-8"))
+        client_connection.close()
+        server_socket.close()
+
+        return auth_code
+
+    def do_oauth(self):
+
+        uri = "https://www.dropbox.com/oauth2/authorize"
+
+        datos = {'response_type': 'code',
+                 'client_id': app_key,
+                 'redirect_uri': redirect_uri}
+
+        datos_encoded = urllib.parse.urlencode(datos)
+
+        print ("\tOpenning browser...")
+
+        webbrowser.open_new_tab ((uri + '?' + datos_encoded))
+
+        print ("# Step 4: Handle the OAuth 2.0 server response")
+
+        auth_code = Dropbox.local_server(self)
+
+        print ("# Step 5: Exchange authorization code for refresh and access tokens")
+
+        uri = 'https://api.dropboxapi.com/oauth2/token'
+
+        cabeceras = {'Content-Type': 'application/x-www-form-urlencoded'}
+
+        datos = {'code': auth_code,
+                 'client_id': app_key,
+                 'client_secret': app_secret,
+                 'redirect_uri': redirect_uri,
+                 'grant_type': 'authorization_code'}
+
+        respuesta = requests.post (uri, headers=cabeceras, data=datos, allow_redirects=False)
+        status = respuesta.status_code
+        print ("\tStatus: " + str(status))
+
+        contenido = respuesta.text
+        print ("\tContenido:")
+        print (contenido)
+        contenido_json = json.loads (contenido)
+        self._access_token = contenido_json ['access_token']
+        print ("\taccess_token: " + self._access_token)
+
+        self._root.destroy()
+
+    def list_folder(self, msg_listbox):
+
+        print("/list_folder")
+
+        uri = 'https://api.dropboxapi.com/2/files/list_folder'
+        # https://www.dropbox.com/developers/documentation/http/documentation#files-list_folder
+
+        headers = {'Authorization': 'Bearer ' + self._access_token,
+                   'Content-Type': 'application/json'}
+
+        data = {"path": self._path if self._path != '/' else '',
+                 "recursive": False,
+                 "include_deleted": False,
+                 "include_has_explicit_shared_members": False,
+                 "include_mounted_folders": True,
+                 "include_non_downloadable_files": True}
+
+        datos_json = json.dumps(data)
+        response = requests.post(uri, headers=headers, data=datos_json, allow_redirects=False)
+        status = response.status_code
+        print ("\tStatus: " + str (status))
+        print (response.text)
+
+        self._files = helper.update_listbox2 (msg_listbox, self._path, response.json())
+
+    def transfer_file(self, file_path, file_data):
+
+        print("/upload")
+
+        uri = 'https://content.dropboxapi.com/2/files/upload'
+        # https://www.dropbox.com/developers/documentation/http/documentation#files-upload
+
+        headers = {'Authorization': 'Bearer ' + self._access_token,
+                   'Content-Type': 'application/octet-stream'}
+
+        data = {"path": file_path,
+                 "mode": "add",
+                 "autorename": True,
+                 "mute": False,
+                 "strict_conflict": False
+                 }
+
+        headers['Dropbox-API-Arg'] = json.dumps(data)
+        response = requests.post (uri, headers=headers, data=file_data, allow_redirects=False)
+
+        status = response.status_code
+        print ("\tStatus: " + str (status))
+        print (response.text)
+
+    def delete_file(self, file_path):
+
+        print("/delete_file")
+
+        uri = 'https://api.dropboxapi.com/2/files/delete_v2'
+        # https://www.dropbox.com/developers/documentation/http/documentation#files-delete
+
+        uri = 'https://api.dropboxapi.com/2/files/delete_v2'
+
+        headers = {'Authorization': 'Bearer ' + self._access_token,
+                   'Content-Type': 'application/json'}
+
+        data = {"path": file_path}
+        json_data = json.dumps(data)
+
+        response = requests.post (uri, headers=headers, data=json_data, allow_redirects=False)
+        status = response.status_code
+        print ("\tStatus: " + str (status))
+        print (response.text)
+
+    def create_folder(self, path):
+
+        print("/create_folder")
+
+        # https://www.dropbox.com/developers/documentation/http/documentation#files-create_folder
+
+        uri = 'https://api.dropboxapi.com/2/files/create_folder_v2'
+
+        headers = {'Authorization': 'Bearer ' + self._access_token,
+                   'Content-Type': 'application/json'}
+
+        data = {"path": path,
+                "autorename": False}
+
+        json_data = json.dumps (data)
+
+        response = requests.post (uri, headers=headers, data=json_data, allow_redirects=False)
+        status = response.status_code
+        print ("\tStatus: " + str (status))
+        print (response.text)
+
+    def download(self,path):
+
+        print("/download_file")
+
+        uri = 'https://api.dropboxapi.com/2/files/get_temporary_link'
+
+        headers = {'Authorization': 'Bearer ' + self._access_token,
+                   'Content-Type': 'application/json'}
+
+        data = {"path": path}
+
+        json_data = json.dumps (data)
+
+        response = requests.post (uri, headers=headers, data=json_data, allow_redirects=False)
+        status = response.status_code
+        print ("\tStatus: " + str (status))
+
+        if (status == 200):
+            messagebox.showinfo ("Alert Message", "File Downloaded!")
+
+        content = json.loads (response.content)
+        webbrowser.open(content['link'])
+
+
+
---
+++ P4-Egela2Dropbox/P4_egela2Dropbox/eGela.py
@@ -0,0 +1,255 @@
+# -*- coding: UTF-8 -*-
+from tkinter import messagebox
+import requests
+import urllib
+from bs4 import BeautifulSoup
+import time
+import helper
+
+class eGela:
+
+    _login = 0
+    _cookie = ""
+    _refs = []
+    _root = None
+
+    def __init__(self, root):
+        self._root = root
+
+    def check_credentials(self, username, password, event=None):
+
+        popup, progress_var, progress_bar = helper.progress("check_credentials", "Logging into eGela...")
+        progress = 0
+        progress_var.set(progress)
+        progress_bar.update()
+
+        #-------------------------------------------------------------
+
+        print("##### 1. PETICION #####")
+        metodo = 'POST'
+        uri = "https://egela.ehu.eus/login/index.php"
+
+        cabeceras = {'Host': uri.split ('/') [2],
+                     'Content-Type': 'application/x-www-form-urlencoded', }
+
+        data = {'username': username.get(),
+                'password': password.get(), }
+
+        data_encoded = urllib.parse.urlencode (data)
+        cabeceras ['Content-Length'] = str (len (data_encoded))
+        respuesta = requests.request (metodo, uri, headers=cabeceras, data=data_encoded, allow_redirects=False)
+
+        codigo = respuesta.status_code
+        descripcion = respuesta.reason
+
+        print (str (codigo) + " " + descripcion)
+
+        for cabecera in respuesta.headers:
+            print (cabecera + ": " + respuesta.headers [cabecera])
+
+        print ("")
+
+
+        progress = 33
+        progress_var.set(progress)
+        progress_bar.update()
+        time.sleep(1)
+
+        #------------------------------------------------------------------------
+
+        print("\n##### 2. PETICION #####")
+
+        metodo = 'GET'
+        uri = respuesta.headers ['Location']
+        print ('Metodo: ' + metodo)
+        print ('Uri: ' + uri + '\n')
+        cookie = respuesta.headers ['Set-Cookie'].split (',') [0]
+        cabeceras = {'Host': uri.split ('/') [2],
+                     'Cookie': cookie}
+        data = ''
+        data_encoded = urllib.parse.urlencode (data)
+        cabeceras ['Content-Length'] = str (len (data_encoded))
+        respuesta = requests.request (metodo, uri, headers=cabeceras, data=data_encoded, allow_redirects=False)
+
+        codigo = respuesta.status_code
+        descripcion = respuesta.reason
+
+        print (str (codigo) + " " + descripcion)
+
+        for cabecera in respuesta.headers:
+            print (cabecera + ": " + respuesta.headers [cabecera])
+
+        print ("")
+
+        progress = 66
+        progress_var.set(progress)
+        progress_bar.update()
+        time.sleep(1)
+
+        #-------------------------------------------------------------------
+
+        print("\n##### 3. PETICION #####")
+
+
+        if respuesta.headers.__contains__('Location'):
+
+
+            metodo = 'GET'
+            uri = respuesta.headers ['Location']
+            print ('Metodo: ' + metodo)
+            print ('Uri: ' + uri + '\n')
+
+
+            cabeceras = {'Host': uri.split ('/') [2],
+                         'Cookie': cookie}
+
+            data = ''
+            data_encoded = urllib.parse.urlencode (data)
+            cabeceras ['Content-Length'] = str (len (data_encoded))
+            respuesta = requests.request (metodo, uri, headers=cabeceras, data=data_encoded, allow_redirects=False)
+
+            codigo = respuesta.status_code
+            descripcion = respuesta.reason
+            contenido = respuesta.content
+
+            print (str (codigo) + " " + descripcion)
+
+            for cabecera in respuesta.headers:
+                print (cabecera + ": " + respuesta.headers [cabecera])
+
+            print ("")
+
+            progress = 100
+            progress_var.set(progress)
+            progress_bar.update()
+            time.sleep(1)
+            popup.destroy()
+
+            if codigo==200:
+
+                self._login = 1
+                self._cookie = cookie
+                self._root.destroy()
+                print("Login correct!")
+
+        else:
+            messagebox.showinfo("Alert Message", "Login incorrect!")
+
+
+    def get_pdf_refs(self):
+
+        popup, progress_var, progress_bar = helper.progress("get_pdf_refs", "Downloading PDF list...")
+        progress = 0
+        progress_var.set(progress)
+        progress_bar.update()
+
+        print("\n##### 4. PETICION (Página principal de la asignatura en eGela) #####")
+
+
+        metodo = 'GET'
+        uri = 'https://egela.ehu.eus/course/view.php?id=43994'
+        print ('Metodo: ' + metodo)
+        print ('Uri: ' + uri + '\n')
+        cabeceras = {'Host': uri.split ('/') [2],
+                     'Cookie': self._cookie}
+        data = ''
+        data_encoded = urllib.parse.urlencode (data)
+        cabeceras ['Content-Length'] = str (len (data_encoded))
+        respuesta = requests.request (metodo, uri, headers=cabeceras, data=data_encoded, allow_redirects=False)
+
+        codigo = respuesta.status_code
+        descripcion = respuesta.reason
+        contenido = respuesta.content
+
+        print (str (codigo) + " " + descripcion)
+
+        for cabecera in respuesta.headers:
+            print (cabecera + ": " + respuesta.headers [cabecera])
+
+        print ("")
+
+
+        print("\n##### Analisis del HTML... #####")
+
+
+        soup = BeautifulSoup (contenido, 'html.parser')
+
+        links = soup.find_all ('a')
+
+        progress_step = float (100.0 / len (links))
+
+        filelink = ''
+        self._refs =[]
+
+        for link in links:
+            imgs = link.find_all ('img')
+            for img in imgs:
+                if ('pdf' in img ['src']):
+                    filelink = link.get ('href')
+                    pdf_name = link.span.text
+
+                    self._refs.append({'pdf_name': pdf_name , 'pdf_link': filelink})
+
+                    progress += progress_step
+                    progress_var.set(progress)
+                    progress_bar.update()
+                    time.sleep(0.1)
+
+
+        popup.destroy()
+        return self._refs
+
+
+    def get_pdf(self, selection):
+
+        print("\t##### descargando  PDF... #####")
+
+        metodo = 'GET'
+        uri = self._refs[selection]['pdf_link']
+        print ('Metodo: ' + metodo)
+        print ('Uri: ' + uri + '\n')
+        cabeceras = {'Host': uri.split ('/') [2], 'Cookie': self._cookie}
+        data = ''
+        data_encoded = urllib.parse.urlencode (data)
+        cabeceras ['Content-Length'] = str (len (data_encoded))
+        respuesta = requests.request (metodo, uri, headers=cabeceras, data=data_encoded, allow_redirects=False)
+
+        codigo = respuesta.status_code
+        descripcion = respuesta.reason
+
+        print (str (codigo) + " " + descripcion)
+
+        for cabecera in respuesta.headers:
+            print (cabecera + ": " + respuesta.headers [cabecera])
+
+        print("")
+
+        metodo = 'GET'
+        uri = respuesta.headers ['Location']
+        print ('Metodo: ' + metodo)
+        print ('Uri: ' + uri + '\n')
+        cabeceras = {'Host': uri.split ('/') [2], 'Cookie': self._cookie}
+        data = ''
+        data_encoded = urllib.parse.urlencode (data)
+        cabeceras ['Content-Length'] = str (len (data_encoded))
+        respuesta = requests.request (metodo, uri, headers=cabeceras, data=data_encoded, allow_redirects=False)
+
+        codigo = respuesta.status_code
+        descripcion = respuesta.reason
+        contenido = respuesta.content
+
+        print (str (codigo) + " " + descripcion)
+
+        for cabecera in respuesta.headers:
+            print (cabecera + ": " + respuesta.headers [cabecera])
+
+        print ("")
+
+        pdf_name = self._refs[selection]['pdf_name'] + ".pdf"
+        pdf_content = respuesta.content
+
+        print ("File " + pdf_name + " downloaded")
+
+
+        return pdf_name, pdf_content
+
